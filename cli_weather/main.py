692916cebd69e3415f52f26dc3a40f474f3967cf
vatsa287 <i.mnshreevatsa@gmail.com>
2020-07-25 00:09:36 +0530

    main + subparsers

---
+++ cli_weather/main.py
@@ -0,0 +1,13 @@
+import argparse
+from get_by_city import get_by_city_args,city_parse
+
+def main():
+    parser = argparse.ArgumentParser('cli-weather')
+    subparsers = parser.add_subparsers()
+    get_by_city_args(subparsers)
+    args = parser.parse_args()
+    if args.city is not None:
+        city_parse(args)
+
+if __name__ == "__main__":
+    main()
