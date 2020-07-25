aa8fa99305ce5c7a73efe4f525b26e8eb23d4d07
vatsa287 <i.mnshreevatsa@gmail.com>
2020-07-25 14:54:04 +0530

    get_by_city.py subparsers

--- cli_weather/get_by_city.py
+++ cli_weather/get_by_city.py
@@ -1,22 +1,25 @@
 import requests
-import json
-import argparse
 
 def get_by_city_args(subparsers):
     city_parser = subparsers.add_parser('city')
     city_parser.add_argument(
         "city",
-        help = "enter city name to get current temparature"
+        help="enter city name to get current temparature"
     )
     city_parser.add_argument(
         "-c", "--country",
-        help = "country of the city",
+        help="country of the city",
         default=""
     )
     city_parser.add_argument(
         "-u", "--units",
-        help = "M - [DEFAULT] Metric (Celcius, m/s, mm), S - Scientific (Kelvin, m/s, mm),I - Fahrenheit (F, mph, in)",
+        help="M - [DEFAULT] Metric (Celcius, m/s, mm), S - Scientific (Kelvin, m/s, mm),I - Fahrenheit (F, mph, in)",
         default="M"
+    )
+    city_parser.add_argument(
+        "-d","--detailed",
+        help="detailed weather data",
+        action="store_true"
     )
 
 def city_parse(args):
@@ -39,13 +42,47 @@ def city_parse(args):
 
     response = requests.request("GET", url, params=querystring)
 
-    main_data = response.json()
-    data = main_data['data']
+    try:
+        main_data = response.json()
+    # ValueError-unable to decode json, UnboundLocalError-used var before declaring
+    except (ValueError,UnboundLocalError) as err:
+        print("Invalid city")
+        print("Please use format ex: $ cli-weather city_name [-c country_name][-u S/M/F] [-d]")
+        return
 
+    data = main_data['data']
+    weather = data[0]['weather']
+    # defalut metric values
     degree = "celcius"
+    speed = "m/s"
+
     if args.units == "S":
         degree = "kelvin"
     elif args.units == "F":
         degree = "Fahrenheit"
+        speed = "mph"
 
-    print("Current temparature in {} is {} {}" .format(city.capitalize(), data[0]['temp'], degree))
+    if args.detailed:
+        print(
+            " Have a nice day!\n\n",
+            "city: {}\n".format(args.city.capitalize()),
+            "temparature: {} {}\n".format(data[0]['temp'],degree),
+            "summary: {}\n".format(weather['description']),
+            "source station ID: {}\n".format(data[0]['station']),
+            "latitude: {} degrees\n".format(data[0]['lat']),
+            "longitude: {} degrees\n".format(data[0]['lon']),
+            "timezone: {}\n".format(data[0]['timezone']),
+            "last observation time: {}\n".format(data[0]['ob_time']),
+            "sunrise: {}\n".format(data[0]['sunrise']),
+            "sunset: {}\n".format(data[0]['sunset']),
+            "pressure: {} mb\n".format(data[0]['pres']),
+            "sea level pressure: {} mb\n".format(data[0]['slp']),
+            "wind speed: {} {}\n".format(data[0]['wind_spd'],speed),
+            "wind direction: {} degrees\n".format(data[0]['wind_dir']),
+            "visibility: {} KM\n".format(data[0]['vis']),
+            "relative humidity: {} %\n".format(data[0]['rh']),
+            "estimated solar radiation: {} W/m^2".format(data[0]['solar_rad'])
+        )
+    elif args.detailed is False:
+        print("Current temparature in {} is {} {}" .format(city.capitalize(), data[0]['temp'], degree))
+    
